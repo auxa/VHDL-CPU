5bb68c596cc71151c64475d651d981e5a466d33f
auxa <auxa@users.noreply.github.com>
2016-04-07 13:09:58 +0100

    Update memory.vhd

--- memory.vhd
+++ memory.vhd
@@ -5,29 +5,10 @@
 -- Create Date:    10:47:51 03/29/2016 
 -- Design Name: 
 -- Module Name:    memory - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity memory is
 	Port ( address : in std_logic_vector(15 downto 0);
@@ -35,10 +16,8 @@ entity memory is
 			MemWrite, MemRead : in std_logic;
 			read_data : out std_logic_vector(15 downto 0));
 end memory;
-
 architecture Behavioral of memory is
 type mem_array is array(0 to 511) of std_logic_vector(15 downto 0);
-
 begin
 
 	mem_process: process (address, write_data)
@@ -67,8 +46,6 @@ begin
 	X"0803",
 	X"1403", 
 	X"0803", 
-
-	
 	others=> X"0000");
 	variable addr:integer;
 	begin
@@ -80,4 +57,3 @@ begin
 	end if;
 	end process;
 end Behavioral;
-
